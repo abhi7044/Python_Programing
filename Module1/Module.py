33e3d5203527e353077594eb4827058f4f0af806
Abhijeet Mishra <teslanikola664@gmail.com>
2025-08-30 01:35:59 +0530

    Add files via upload
    In this module i done or learn Module , Comments and pip . In this module i done my sheet problem also in that there was a module which speak whatever you write.

---
+++ Module1/Module.py
@@ -0,0 +1,9 @@
+import pyjokes # this is a module of a python 
+joke = pyjokes.get_joke()   # this is a joke use by pujokes module 
+print (joke)
+# print("Hello world")  #---->this is the comment
+"""
+so thanks 
+that was program
+this is comment for multiple lines
+"""
---
+++ Module1/Practice_Sheet.py
@@ -0,0 +1,36 @@
+import pyttsx3
+# Question 1 : write a program of poem 
+print('''
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the trav'ller in the dark,
+Thanks you for your tiny spark,
+He could not see which way to go,
+If you did not twinkle so.
+
+In the dark blue sky you keep,
+And often thro' my curtains peep,
+For you never shut your eye,
+Till the sun is in the sky.
+
+'Tis your bright and tiny spark,
+Lights the trav'ller in the dark:
+Tho' I know not what you are,
+Twinkle, twinkle, little star.
+''')
+
+# Question 2 : use repl and print the table of 5 using it
+"""this code will be write in terminal after writting python """
+
+# Question 3 : install external module and use it to perform an operation of you interest
+engine = pyttsx3.init()
+engine.say("Hello World")
+engine.runAndWait()
